45a7ab78e5090602fa8cfd2cb0e8fd196740abfb
Yashi05gupta <guptashweta2705@gmail.com>
2025-01-31 11:43:46 +0530

    Add files via upload

---
+++ arraynew.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    arr =[1,2,0,5,6,0,8,9]
+    temp[]
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            temp[j] = arr[i]
+            j++;
+        }
+        else
+        {continue;
+        }
+    }
+}
